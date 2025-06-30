06ce880bf671fab3549092d6ef0a16684bd79311
Blackburn0 <hamzatadebayo5@gmail.com>
2025-06-30 15:00:10 +0100

    staticfiles in prod

--- src/config/settings.py
+++ src/config/settings.py
@@ -138,15 +138,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+# STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+# STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
-STATICFILES_DIRS = [
-    STATICFILES_BASE_DIR,
+# STATICFILES_DIRS = [
+#     STATICFILES_BASE_DIR,
     
-]
+# ]
 
 
 STATIC_ROOT = BASE_DIR / "local-cdn"

835003b24e9f45e37d2543621fb5e0250139730c
Blackburn0 <hamzatadebayo5@gmail.com>
2025-06-30 15:20:39 +0100

    staticfiles in prod

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -17,5 +17,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
+# application = WhiteNoise(application, root=settings.STATIC_ROOT)
 
